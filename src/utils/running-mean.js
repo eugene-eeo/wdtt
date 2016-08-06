6238e1eb7c0ce462ef331238f0aee9f1825fecc1
Eeo Jun <141bytes@gmail.com>
2016-08-06 22:08:56 +0800

    Throw away unnecessary computation

--- src/utils/running-mean.js
+++ src/utils/running-mean.js
@@ -1,22 +1,15 @@
 var atMost = require('./at-most');
 var sum = a => a.reduce((r, a) => r+a);
 
-function avg(arr) {
-    var len = arr.length;
-    return len > 0
-        ? sum(arr) / len
-        : 0;
-}
-
 module.exports = function() {
     var mean  = 0;
     var count = 0;
-    var history = atMost(15);
+    var history = atMost(20);
     return datum => {
         var prev = mean;
-        mean = ((mean * count) + datum) / ++count;
+        mean = (mean * count + datum) / ++count;
         return {
-            growing: avg(history(mean - prev)) > 0,
+            growing: sum(history(mean - prev)) > 0,
             mean,
         };
     };
