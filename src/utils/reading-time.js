9f4590265643270a3a3d23698c4af173a823feb6
Eeo Jun <141bytes@gmail.com>
2016-08-06 22:29:44 +0800

    Test dimensions function

--- src/utils/reading-time.js
+++ src/utils/reading-time.js
@@ -1,6 +1,6 @@
 module.exports = function(text) {
     // words per minute
-    var wpm = 250;
+    var wpm = 230;
     var spaces = /\W+/g;
     var words = text.split(spaces).length;
     return Math.ceil((words / wpm) * 60);
---
+++ tests/test-dimensions.js
@@ -0,0 +1,39 @@
+var test = require('tape');
+var dimensions = require('../src/utils/dimensions');
+
+
+function extend(a, b) {
+    return Object.assign(a, b);
+}
+
+
+function mockElem(opts) {
+    var zeroPad = {left: 0, right: 0, top: 0, bottom: 0};
+    var zeroBor = {left: false, right: false, top: false, bottom: false};
+    return {
+        height:  100,
+        width:   100,
+        padding: extend(zeroPad, opts.padding || {}),
+        border:  extend(zeroBor, opts.border  || {}),
+    };
+}
+
+
+test('dimensions padding', function(t) {
+    var elem = mockElem({ padding: { left: 1, right: 1 } });
+    t.deepEqual(dimensions(elem), {
+        width: 98,
+        height: 100,
+    });
+    t.end();
+});
+
+
+test('dimensions border', function(t) {
+    var elem = mockElem({border: {left: true, right: true}});
+    t.deepEqual(dimensions(elem), {
+        width: 98,
+        height: 100,
+    });
+    t.end();
+});
