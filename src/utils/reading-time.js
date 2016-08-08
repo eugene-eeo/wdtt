ec5832b289cc92370efc57aad8d7b5dce081d289
Eeo Jun <141bytes@gmail.com>
2016-08-08 19:37:23 +0800

    Add 0.5 second delay by default

--- src/utils/reading-time.js
+++ src/utils/reading-time.js
@@ -3,5 +3,5 @@ module.exports = function(text) {
     var wpm = 230;
     var spaces = /\W+/g;
     var words = text.split(spaces).length;
-    return Math.ceil((words / wpm) * 60);
+    return Math.ceil((words / wpm) * 60) + 0.5;
 };
